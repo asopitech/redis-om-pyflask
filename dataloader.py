43fe947a185880314b9a2d91fe470dfc07589b88
asopitech <asopitech.iot.research+github@gmail.com>
2023-05-22 20:50:48 +0900

    first typerized: default command to typer

--- dataloader.py
+++ dataloader.py
@@ -1,9 +1,18 @@
 import json
 import requests
+import typer
 
-with open('data/people.json', encoding='utf-8') as f:
-    people = json.loads(f.read())
+app = typer.Typer()
 
-for person in people:
-    r = requests.post('http://127.0.0.1:5000/person/new', json = person)
-    print(f"Created person {person['first_name']} {person['last_name']} with ID {r.text}")
+@app.command()
+def main():
+    with open('data/people.json', encoding='utf-8') as f:
+        people = json.loads(f.read())
+
+    for person in people:
+        r = requests.post('http://127.0.0.1:5000/person/new', json = person)
+        print(f"Created person {person['first_name']} {person['last_name']} with ID {r.text}")
+
+
+if __name__ == "__main__":
+    app()
